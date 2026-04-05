3dcc5d9a4cf446f57e5b081bf64bd8feef79fb96
Gilda Market <gildamarket@users.noreply.github.com>
2026-04-05 21:33:12 +0300

    chore: remove backend files from frontend branch

--- backend/src/routes/auth.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const { login } = require('../controllers/authController');
-
-router.post('/telegram', login);
-
-module.exports = router;
--- backend/src/routes/favorites.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { requireAuth } = require('../middleware/auth');
-const ctrl = require('../controllers/favoritesController');
-
-router.get('/', requireAuth, ctrl.getFavorites);
-router.post('/shop/:shopId', requireAuth, ctrl.toggleShopLike);
-
-module.exports = router;
